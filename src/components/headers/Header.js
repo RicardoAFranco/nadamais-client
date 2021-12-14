fc42bd733a64e0ca55867bf6f96303fbce28abb8
Ricardo Franco <ricardofranco14@gmail.com>
2021-12-14 10:37:37 +0000

    resolving issues with deployment

--- src/components/headers/Header.js
+++ src/components/headers/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { GlobalState } from "../../GlobalState";
 import Menu from "./icon/menu.svg";
 import Close from "./icon/close.svg";
@@ -7,7 +7,6 @@ import { Link } from "react-router-dom";
 
 function Header() {
   const value = useContext(GlobalState);
-  console.log(value);
   return (
     <header>
       <div className="menu">
